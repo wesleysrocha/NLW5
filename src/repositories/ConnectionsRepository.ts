6c615d03a9cf3704239151af91c16389fbeffabd
Wesley Rocha <wesleyrocha.it19@gmail.com>
2021-05-25 19:18:44 -0300

    first commit

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,6 @@
+import { Entity, EntityRepository, Repository} from "typeorm";
+import {Connection} from "../entities/Connection";
+
+@EntityRepository(Connection)
+class ConnectionsRepository extends Repository<Connection> {}
+export{ConnectionsRepository}; 
---
+++ src/repositories/MessagesRepository.ts
@@ -0,0 +1,5 @@
+import { Repository, EntityRepository } from "typeorm";
+import {Message} from "../entities/Message";
+@EntityRepository(Message)
+class MessagesRepository extends Repository<Message> {}
+export{MessagesRepository}; 
